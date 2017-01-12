5ecbbb38985ec5fef8fae65f6b1e459a46a97ae3
Alexander Lang <alex@upstre.am>
2017-01-12 16:07:17 +0100

    fixes migration

--- db/migrate/20170112144724_add_access_token_to_spaces.rb
+++ db/migrate/20170112144724_add_access_token_to_spaces.rb
@@ -3,7 +3,7 @@ class AddAccessTokenToSpaces < ActiveRecord::Migration
     add_column :spaces, :access_token, :string
     Space.reset_column_information
     Space.all.each do |space|
-      space.update_attribute :access_token, space.admins.first&.access_token
+      space.update_attribute :access_token, Admin.where(space_id: space.id).first&.access_token
     end
   end
 end
