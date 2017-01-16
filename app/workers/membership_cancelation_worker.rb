520f4b87789b4b7d795e5827cc3e6c39eb221f68
Alexander Lang <alex@upstre.am>
2017-01-16 11:20:11 +0100

    fixes error when team has been deleted

--- app/workers/membership_cancelation_worker.rb
+++ app/workers/membership_cancelation_worker.rb
@@ -2,7 +2,7 @@ class MembershipCancelationWorker
   include Sidekiq::Worker
 
   def perform(team_id, membership)
-    team = Team.find team_id
+    team = Team.where(id: team_id).first || return
     TeamService.new(team).deactivate membership['email'], membership['name']
   end
 end
