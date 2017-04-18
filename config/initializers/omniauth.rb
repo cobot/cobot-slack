188450454dab9c61f990402209be3975a30fe04e
genevievebelle <genevieve@MacBook-Pro.local>
2017-04-18 17:40:19 +0200

    add client to list of oauth scopes

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ require "omniauth/strategies/cobot"
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :cobot, Rails.application.secrets.cobot_client_id,
     Rails.application.secrets.cobot_client_secret,
-      scope: 'read_user read_memberships write',
+      scope: 'read_user read_memberships write client',
       client_options: {site: Rails.application.config.cobot_site,
         authorize_url: "#{Rails.application.config.cobot_site}/oauth/authorize",
         token_url: "#{Rails.application.config.cobot_site}/oauth/access_token"}
