4ab406bd8f9c24b7c429f950e203e5a3093839ef
Alexander Lang <alex@upstre.am>
2020-03-12 12:31:29 +0100

    fix error when admin has no write access to activities fixes SLACK-1Y

--- app/workers/activity_worker.rb
+++ app/workers/activity_worker.rb
@@ -1,9 +1,13 @@
+# frozen_string_literal: true
+
 class ActivityWorker
   include Sidekiq::Worker
 
   def perform(space_id, text, level = nil)
     space = Space.where(id: space_id).first || return
     api_client = CobotClient::ApiClient.new(space.access_token)
-    api_client.post space.subdomain, '/activities', {text: text, level: level}.compact
+    api_client.post space.subdomain, '/activities', { text: text, level: level }.compact
+  rescue CobotClient::Forbidden
+    # unable to post actibity. admin does not have access to write activities.
   end
 end
