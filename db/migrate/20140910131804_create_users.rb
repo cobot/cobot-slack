43989ec1e13fbc0a5c0da86d25f423d738ee911b
Alexander Lang <alex@cobot.me>
2023-04-03 18:00:00 +0200

    change extension

--- db/migrate/20140910131804_create_users.rb
+++ db/migrate/20140910131804_create_users.rb
@@ -1,6 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
-    enable_extension 'uuid-ossp'
+    enable_extension 'pgcrypto'
     create_table :users, id: :uuid do |t|
       t.string :cobot_id
       t.timestamps
